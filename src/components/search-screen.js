12706ae1d55cf9ca569b5c16d9d39028963d5c1c
Dan Yin <danyin@Dans-MacBook-Pro.local>
2021-04-11 16:15:55 -0700

    created viewAllUsers, profile, appliedjobs and reviewedcandidates links in the search page

--- src/components/search-screen.js
+++ src/components/search-screen.js
@@ -3,109 +3,140 @@ import {Link, useParams, useHistory} from "react-router-dom";
 import jobService from "../services/job-service"
 
 const SearchScreen = () => {
-    const history = useHistory()
-    const {description, location} = useParams()
-    const [searchDescription, setSearchDescription] = useState(description)
-    const [searchLocation, setSearchLocation] = useState(location)
-    const [isFullTime, setIsFullTime] = useState(false)
-    const [results, setResults] = useState(undefined)
-    useEffect(() => {
-        setSearchDescription(description)
-        setSearchLocation(location)
-        setIsFullTime(false)
-        findJobs(description, location)
-    }, [])
-    const findJobs = (description, location, isFullTime) => {
-        if (description === undefined || description === "") {
-            description = "+"
-        }
-        if (location === undefined || location === "") {
-            location = "+"
-        }
-        history.push(`/search/${description}/${location}`)
-        jobService.findJobs(description, location, isFullTime)
-            .then((results) => {
-                setResults(results)
-            })
+  const history = useHistory()
+  const {description, location} = useParams()
+  const [searchDescription, setSearchDescription] = useState(description)
+  const [searchLocation, setSearchLocation] = useState(location)
+  const [isFullTime, setIsFullTime] = useState(false)
+  const [results, setResults] = useState(undefined)
+  useEffect(() => {
+    setSearchDescription(description)
+    setSearchLocation(location)
+    setIsFullTime(false)
+    findJobs(description, location)
+  }, [])
+  const findJobs = (description, location, isFullTime) => {
+    if (description === undefined || description === "") {
+      description = "+"
     }
-    const handleSubmit = () => {
-        findJobs(searchDescription, searchLocation, isFullTime);
+    if (location === undefined || location === "") {
+      location = "+"
     }
-    const handleKeypress = e => {
-        //it triggers by pressing the enter key
-        if (e.key === 'Enter') {
-            handleSubmit();
-        }
-    };
-    return(
-        <div>
-            <br/>
-            <h2>Search Screen</h2>
-            <br/>
-            <div className="row">
-                <div className="col-4">
-                    <h6>Job Description</h6>
-                    <input value={searchDescription}
-                           placeholder={'e.g. C'}
-                           onChange={(event) => {
-                               setSearchDescription(event.target.value)
-                           }}
-                           onKeyPress = {
-                               handleKeypress
-                           }
-                           className="form-control"/>
-                </div>
-                <div className="col-4">
-                    <h6>Location</h6>
-                    <input value={searchLocation}
-                           placeholder={'e.g. sf'}
-                           onChange={(event) => {
-                               setSearchLocation(event.target.value)
-                           }}
-                           onKeyPress = {
-                               handleKeypress
-                           }
-                           className="form-control"/>
-                </div>
-                <div className="col-2 relativePosition">
-                    <label className = 'checkboxPosition'>Full Time Only
-                        <input type="checkbox" className = 'checkboxMargin'
-                               defaultChecked={false}
-                               onClick={() => {setIsFullTime(!isFullTime)}}
-                        />
-                    </label>
-                </div>
-                <div className="col-2 relativePosition">
-                    <button
-                        onClick={() => {
-                            findJobs(searchDescription, searchLocation, isFullTime)
-                        }}
-                        className="btn btn-primary btn-block searchButton">
-                        Search
-                    </button>
-                </div>
-            </div>
-            <br/>
-            {
-                results !== undefined &&
-                <>
-                    <ul className="list-group">
-                        {
-                            results.map((result) => {
-                                return(
-                                    <li className="list-group-item">
-                                        <Link to={`/details/${result.id}`}>
-                                            {result.title}
-                                        </Link>
-                                    </li>
-                                )
-                            })
-                        }
-                    </ul>
-                </>
-            }
+    history.push(`/search/${description}/${location}`)
+    jobService.findJobs(description, location, isFullTime)
+    .then((results) => {
+      setResults(results)
+    })
+  }
+  const handleSubmit = () => {
+    findJobs(searchDescription, searchLocation, isFullTime);
+  }
+  const handleKeypress = e => {
+    //it triggers by pressing the enter key
+    if (e.key === 'Enter') {
+      handleSubmit();
+    }
+  };
+  return (
+      <div>
+        <br/>
+        <h2>Search Screen</h2>
+        <br/>
+        <div className="row">
+          <div className="col-sm-4">
+            <h6>Job Description</h6>
+            <input value={searchDescription}
+                   placeholder={'e.g. C'}
+                   onChange={(event) => {
+                     setSearchDescription(event.target.value)
+                   }}
+                   onKeyPress={
+                     handleKeypress
+                   }
+                   className="form-control"/>
+          </div>
+          <div className="col-sm-4">
+            <h6>Location</h6>
+            <input value={searchLocation}
+                   placeholder={'e.g. sf'}
+                   onChange={(event) => {
+                     setSearchLocation(event.target.value)
+                   }}
+                   onKeyPress={
+                     handleKeypress
+                   }
+                   className="form-control"/>
+          </div>
+          <div className="col-sm-2">
+            <label className='col-form-label checkboxPosition'>Full Time Only
+              <input type="checkbox" className='checkboxMargin'
+                     defaultChecked={false}
+                     onClick={() => {
+                       setIsFullTime(!isFullTime)
+                     }}
+              />
+            </label>
+          </div>
+          <div className="col-sm-2">
+            <button
+                onClick={() => {
+                  findJobs(searchDescription, searchLocation, isFullTime)
+                }}
+                className="btn btn-primary btn-block searchButton">
+              Search
+            </button>
+          </div>
         </div>
-    )
+        <br/>
+        <div className="row">
+
+          <div className="col-sm-3">
+            <Link className="allUsers" to={`/details/`}>
+              View All Users
+            </Link>
+          </div>
+          <div className="col-sm-3">
+
+            <Link className="searchMenu" to={`/profile/123`}>
+              My profile
+            </Link>
+          </div>
+          <div className="col-sm-3">
+            <Link className="searchMenu" to={`/details/`}>
+              My Applied Jobs
+            </Link>
+          </div>
+
+          <div className="col-sm-3">
+            <Link className="searchMenu" to={`/details/`}>
+              My Saved Candidates
+            </Link>
+          </div>
+        </div>
+
+
+        <br/>
+        <br/>
+        {
+          results !== undefined &&
+          <>
+            <ul className="list-group">
+              {
+                results.map((result) => {
+                  return (
+                      <li className="list-group-item">
+                        <Link to={`/details/${result.id}`}>
+                          {result.title}
+                        </Link>
+                      </li>
+                  )
+                })
+              }
+            </ul>
+          </>
+        }
+      </div>
+  )
 }
 
 export default SearchScreen
