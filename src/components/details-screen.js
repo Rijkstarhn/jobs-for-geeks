10ce40c2ba129755029ae57ede41c818764077c3
Dan Yin <danyin@Dans-MacBook-Pro.local>
2021-04-23 13:20:30 -0700

    added modal feedback when user successfully saves the job

--- src/components/details-screen.js
+++ src/components/details-screen.js
@@ -70,8 +70,37 @@ const DetailsScreen = ({user, create, status}) => {
         </button>
 
         {user.role === "JOB SEEKER" && status === LOGIN_STATE.LOGGED_IN &&
-        <button className="btn btn-primary float-end"
-                onClick={() => handleCreateJob()}>Add to my job list</button>}
+        // <button className="btn btn-primary float-end"
+        //         onClick={() => handleCreateJob()}>Add to my job list</button>
+
+          <button type="button" className="btn btn-primary float-end" data-bs-toggle="modal"
+          data-bs-target="#exampleModal" onClick={() => handleCreateJob()}>
+          Add to my job list modal
+          </button>
+        }
+
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel"></h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+                You have successfully added this job to your saved job list!
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+
+
+
+
       </div>
   )
 }
